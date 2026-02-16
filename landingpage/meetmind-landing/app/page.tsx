ec27c7b1861f62eb20950513cddac095ff008264
xenon <demiladeafuape74@gmail.com>
2026-02-16 16:16:47 +0100

    updated th epage to pass the open function to navbar

--- landingpage/meetmind-landing/app/page.tsx
+++ landingpage/meetmind-landing/app/page.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState } from "react";
 import Footer from "./components/layout/Footer";
 import Navbar from "./components/layout/Navbar";
 import CTA from "./components/sections/CTA";
@@ -7,15 +10,22 @@ import Problem from "./components/sections/Problem";
 import Solution from "./components/sections/Solution";
 
 export default function Home() {
+  // 1. Create the global state for the waitlist modal
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <main>
-      {/* <Navbar/> */}
-      <Hero />
+    <main className="bg-zinc-950">
+      {/* 2. Pass the 'open' function to the Navbar */}
+      <Navbar onJoinClick={() => setIsModalOpen(true)} />
+
+      {/* 3. Pass both the state and setter to the Hero */}
+      <Hero isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} />
+
       <Problem />
       <Solution />
       <Demo />
       <CTA />
-      <Footer/>
+      <Footer />
     </main>
   );
 }
