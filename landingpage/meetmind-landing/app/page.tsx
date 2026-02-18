175bab9b1b349d406255f4f0c64bb009bfce6c49
xenon <demiladeafuape74@gmail.com>
2026-02-18 02:27:46 +0100

    one more change to  page layout

--- landingpage/meetmind-landing/app/page.tsx
+++ landingpage/meetmind-landing/app/page.tsx
@@ -24,7 +24,7 @@ export default function Home() {
       {/* <Problem /> */}
       {/* <Solution /> */}
       {/* <Demo /> */}
-      <CTA />
+      {/* <CTA /> */}
       <Footer />
     </main>
   );
